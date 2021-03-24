d0069353c5418e4b1a688a85ffac8cb16fe61a71
Raf Vanpuyvelde <raf.vanpuyvelde@sympl.be>
2021-03-24 13:05:12 +0100

    ✨ Adds skin typeDefs

--- packages/api/src/weapons/typeDef.ts
+++ packages/api/src/weapons/typeDef.ts
@@ -4,6 +4,14 @@ const typeDef = gql`
   type Weapon {
     uuid: ID
     displayName: String
+    displayIcon: String
+    skins: [Skin]
+  }
+
+  type Skin {
+    uuid: String
+    displayName: String
+    displayIcon: String
   }
 
   extend type Query {

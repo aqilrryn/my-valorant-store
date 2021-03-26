9ffc150821b706d35a676521e35ecfca2fa409a9
Raf Vanpuyvelde <raf.vanpuyvelde@sympl.be>
2021-03-26 09:13:47 +0100

    ✨ Adds display icon to store gql

--- packages/api/src/store/Store.ts
+++ packages/api/src/store/Store.ts
@@ -1,14 +1,18 @@
 import { Store as StoreType } from "./types";
 import { getPlayerStore } from "../services/rsoService";
+import Skin from "../skins/Skin";
 
 abstract class Store {
   static async playerStore(): Promise<StoreType> {
     const store = await getPlayerStore();
+    const skins = await Skin.all();
 
     return {
       skins: (store ?? []).map((s) => ({
         uuid: s?.id ?? "",
         displayName: s?.name ?? "",
+        displayIcon: skins.find((skin) => skin.displayName === s?.name)
+          ?.displayIcon,
       })),
     };
   }

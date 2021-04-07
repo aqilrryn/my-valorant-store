a903a6f5116c99f98c6368278a74015d8c2299af
Raf Vanpuyvelde <raf.vanpuyvelde@sympl.be>
2021-04-07 16:24:23 +0200

    ✨ Updates package

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,27 @@
-import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
 
-import skins from './skins';
-import store from './store';
-import weapons from './weapons';
+import skins from "./skins";
+import store from "./store";
+import weapons from "./weapons";
 
-async function startApolloServer() {
-  const typeDef = gql`
-    type Query
-  `;
+const typeDef = gql`
+  type Query
+`;
 
-  const server = new ApolloServer({
-    typeDefs: [typeDef, weapons.typeDef, skins.typeDef, store.typeDef],
-    resolvers: [weapons.resolvers, skins.resolvers, store.resolvers],
-    introspection: true,
-    playground: true,
-  });
+const server = new ApolloServer({
+  typeDefs: [typeDef, weapons.typeDef, skins.typeDef, store.typeDef],
+  resolvers: [weapons.resolvers, skins.resolvers, store.resolvers],
+  introspection: true,
+  playground: true,
+});
 
-  await server.start();
-
+server.start().then(() => {
   const app = express();
   server.applyMiddleware({ app });
 
-  await new Promise(resolve =>
-    app.listen({ port: process.env.PORT || 4000 }, () => resolve),
-  );
-
-  console.log(`🚀 Server ready at url/${server.graphqlPath}`);
-
-  return { server, app };
-}
-
-async () => await startApolloServer();
+  new Promise((resolve) =>
+    // @ts-ignore
+    app.listen({ port: process.env.PORT || 4000 }, resolve)
+  ).then(() => console.log(`🚀 Server ready at url${server.graphqlPath}`));
+});
